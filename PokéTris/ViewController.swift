d7dfae00b1ac54a2aa6096cade2531f167c8fd74
g551 DIT UPM <g551@l139.lab.dit.upm.es>
2016-10-03 12:22:16 +0200

    Initial Commit

---
+++ PokéTris/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokéTris
+//
+//  Created by g551 DIT UPM on 3/10/16.
+//  Copyright © 2016 g551 DIT UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PokéTrisTests/Poke_TrisTests.swift
@@ -0,0 +1,36 @@
+//
+//  Poke_TrisTests.swift
+//  PokéTrisTests
+//
+//  Created by g551 DIT UPM on 3/10/16.
+//  Copyright © 2016 g551 DIT UPM. All rights reserved.
+//
+
+import XCTest
+@testable import Poke_Tris
+
+class Poke_TrisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
